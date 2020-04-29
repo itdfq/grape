e931668d53528322ed6b687dd6a3af97e5951d23
90925 <909256107@qq.com>
2020-04-29 19:38:54 +0800

    添加拦截器

---
+++ src/main/java/com/dfq/grape/config/LoginConfig.java
@@ -0,0 +1,28 @@
+package com.dfq.grape.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Mr Duan
+ * @create 2020-04-29 19:31
+ */
+@Configuration
+public class LoginConfig  implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //注册TestInterceptor拦截器
+        InterceptorRegistration registration = registry.addInterceptor(new AdminInterceptor());
+        registration.addPathPatterns("/**");                      //所有路径都被拦截
+        registration.excludePathPatterns(                         //添加不拦截路径
+                "你的登陆路径",            //登录
+                "/**/*.html",            //html静态资源
+                "/**/*.js",              //js静态资源
+                "/**/*.css",             //css静态资源
+                "/**/*.woff",
+                "/**/*.ttf"
+        );
+    }
+}
